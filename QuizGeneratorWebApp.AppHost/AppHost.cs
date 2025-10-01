110e96be545968fdd0c2c9543d076c6a294480b0
Rumcajs121 <z.miara45@gmail.com>
2025-10-01 22:16:35 +0200

    feat(StructureProject)I created structures form my engineenering project

---
+++ QuizGeneratorWebApp.AppHost/AppHost.cs
@@ -0,0 +1,15 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+builder.AddProject<Projects.ContextBuilderService>("contextbuilderservice");
+
+builder.AddProject<Projects.LLMService>("llmservice");
+
+builder.AddProject<Projects.QuizGeneratorWebApp>("quizgeneratorwebapp");
+
+builder.AddProject<Projects.QuizService>("quizservice");
+
+builder.AddProject<Projects.UserService>("userservice");
+
+builder.AddProject<Projects.YarpGateway>("yarpgateway");
+
+builder.Build().Run();
