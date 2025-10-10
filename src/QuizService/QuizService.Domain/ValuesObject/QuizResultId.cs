1400b5d372089b7d9e1d145664c15af8b383eace
Rumcajs121 <z.miara45@gmail.com>
2025-10-10 20:19:34 +0200

    feat(DDD) Created Result model

---
+++ src/QuizService/QuizService.Domain/ValuesObject/QuizResultId.cs
@@ -0,0 +1,20 @@
+using QuizService.Domain.Exceptions;
+
+namespace QuizService.Domain.ValuesObject;
+
+public class QuizResultId
+{
+    public Guid Value { get;}
+
+    private QuizResultId(Guid value)=>Value = value;
+
+    public static QuizResultId Of(Guid value)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+        if (value == Guid.Empty)
+        {
+            throw new DomainException("QuizResultId cannot be empty");
+        }
+        return new QuizResultId(value);
+    }
+}
