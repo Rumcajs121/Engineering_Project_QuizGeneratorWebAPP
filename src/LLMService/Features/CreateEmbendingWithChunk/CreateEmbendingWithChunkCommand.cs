b2812ff2e30c15a60629c7f6d3d9cb44feeab166
Rumcajs121 <z.miara45@gmail.com>
2025-12-04 21:25:15 +0100

    feat(LLMService) Start create CQRS with LLMService

--- src/LLMService/Features/CreateEmbendingWithChunk/CreateEmbendingWithChunkCommand.cs
+++ src/LLMService/Features/CreateEmbendingWithChunk/CreateEmbendingWithChunkCommand.cs
@@ -1,6 +1,15 @@
+using System.Windows.Input;
+using BuildingBlocks.CQRS;
+
 namespace LLMService.Features.CreateEmbendingWithChunk;
 
-public class CreateEmbendingWithChunkCommand
+public record CreateEmbendingWithChunkCommandResponse(bool Success);
+public record CreateEmbendingWithChunkCommand(string DataJson):ICommand<CreateEmbendingWithChunkCommandResponse>;
+
+public class CreateEmbendingWithChunkCommandHandler:ICommandHandler<CreateEmbendingWithChunkCommand,CreateEmbendingWithChunkCommandResponse>
 {
-    
+    public Task<CreateEmbendingWithChunkCommandResponse> Handle(CreateEmbendingWithChunkCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
 }
