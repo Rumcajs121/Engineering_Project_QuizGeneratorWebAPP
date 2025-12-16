e1820a8788ed0449089f0124b0d93f501158f588
Rumcajs121 <z.miara45@gmail.com>
2025-12-16 16:10:06 +0100

    repair .gitignore add appsettings

--- src/ContextBuilderService/Features/GetDataAndChunking/GetDataAndChunkingService.cs
+++ src/ContextBuilderService/Features/GetDataAndChunking/GetDataAndChunkingService.cs
@@ -26,7 +26,7 @@ public class GetDataAndChunkingService(IRepository repository):IGetDataAndChunki
             tokenCounter: null
         );
         var totalChunks = chunks.Count;
-        var documentId = Guid.NewGuid(); 
+        var documentId = Guid.NewGuid(); //TODO: Replace with actual document ID if available 
         var chunkModel = chunks.Select((content, index) => new Chunk(
             DocumentId: documentId,
             ChunkIndex: index,
